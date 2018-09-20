722bc2698c47e0c72ca7354935fdb621bbed5edc
Arthur Vieira Moreira Lucena <arthurlucenapj@gmail.com>
2018-09-20 10:20:31 -0300

    Classe de command

--- java/work/src/br/ufu/sd/work/util/Command.java
+++ java/work/src/br/ufu/sd/work/util/Command.java
@@ -1,13 +1,49 @@
 package br.ufu.sd.work.util;
 
 public class Command {
-    private String nome;
+    private String name;
+    private String[] args;
+    private int idClient;
+    private long timeStamp;
+    private boolean executed;
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+
+    public void setArgs(String[] args) {
+        this.args = args;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public boolean getExecuted() {
+        return executed;
+    }
+
+    public void setExecuted(boolean executed) {
+        this.executed = executed;
     }
 }
